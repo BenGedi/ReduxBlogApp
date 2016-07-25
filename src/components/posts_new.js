a4ffb16fe438845511e8bdd2c31d3b39dce6ea32
Ben Gedi <bengedi@gmail.com>
2016-07-25 03:21:20 +0300

    update form fields with this redux form initial properties

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,20 +3,26 @@ import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
     render() {
+        // this es6 syntax:
+        const { fields: { title, categories, content }, handleSubmit } = this.props;
+        // replacing this es5 syntax:
+        // const title = this.props.fields.title;
+        // ....
+
         return (
-            <form>
+            <form onSubmit={ handleSubmit }>
                 <h3>Create A New Post</h3>
                 <div className="form-group">
                     <label>Title</label>
-                    <input type="text"className="form-control" />
+                    <input type="text"className="form-control" { ...title } />
                 </div>
                 <div className="form-group">
                     <label>Categories</label>
-                    <input type="text"className="form-control" />
+                    <input type="text"className="form-control" { ...categories } />
                 </div>
                 <div className="form-group">
                     <label>Content</label>
-                    <textarea className="form-control" />
+                    <textarea className="form-control" { ...contnet } />
                 </div>
                 <button type="submit" className="btn btn-primary" >Submit</button>
             </form>
@@ -25,10 +31,10 @@ class PostsNew extends Component {
 }
 
 // initial reduxForm
-export default reduxForm(
+export default reduxForm({
     form: 'PostsNewFrom',
     fields: ['title','categories', 'content']
-)(PostsNew);
+})(PostsNew);
 
 
 // when user is typing something... redux-form record it on application state as follows:
