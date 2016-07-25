7ac7c4a868c5cd31af5075d2b69a04a18ffda96e
Ben Gedi <bengedi@gmail.com>
2016-07-25 03:54:33 +0300

    add validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
     render() {
@@ -10,11 +11,14 @@ class PostsNew extends Component {
         // ....
 
         return (
-            <form onSubmit={ handleSubmit }>
+            <form onSubmit={ handleSubmit( this.props.createPost ) }>
                 <h3>Create A New Post</h3>
                 <div className="form-group">
                     <label>Title</label>
                     <input type="text"className="form-control" { ...title } />
+                    <div className="text-help">
+                        { title.touched ? title.error : '' }
+                    </div>
                 </div>
                 <div className="form-group">
                     <label>Categories</label>
@@ -22,7 +26,7 @@ class PostsNew extends Component {
                 </div>
                 <div className="form-group">
                     <label>Content</label>
-                    <textarea className="form-control" { ...contnet } />
+                    <textarea className="form-control" { ...content } />
                 </div>
                 <button type="submit" className="btn btn-primary" >Submit</button>
             </form>
@@ -30,11 +34,30 @@ class PostsNew extends Component {
     }
 }
 
+// before submit, redux form is passing the config props to 'validate' method,
+// if they are not validate the return value is not be empty and a validation will be display
+function validate(values) {
+    const errors = {};
+
+    if(!values.title) {
+        errors.title = 'Enter a username';
+    }
+
+    return errors;
+}
+
 // initial reduxForm
+// redux form has the exact same behavior as connect,
+// it can be use to inject actionsCreator into the component
+
+// the difference between redux-form and connect is:
+// connect: 1st argument is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
 export default reduxForm({
     form: 'PostsNewFrom',
-    fields: ['title','categories', 'content']
-})(PostsNew);
+    fields: ['title','categories', 'content'],
+    validate
+}, null, { createPost })(PostsNew);
 
 
 // when user is typing something... redux-form record it on application state as follows:
